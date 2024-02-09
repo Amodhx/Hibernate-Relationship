85ec60d5506d8b3bb9e047e8071f9912f2483bbf
Amodhx <amxdhnanditha@gmail.com>
2024-02-09 11:25:10 +0530

    hql insert

--- sql/src/main/java/lk/ijse/Main.java
+++ sql/src/main/java/lk/ijse/Main.java
@@ -1,8 +1,14 @@
 package lk.ijse;
 
 import lk.ijse.config.FactoryConfiguration;
+import lk.ijse.entity.Laptop;
+import lk.ijse.entity.Student;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.mapping.Value;
+import org.hibernate.query.Query;
+import org.hibernate.query.QueryParameter;
+import org.hibernate.sql.ast.tree.insert.Values;
 
 import java.util.List;
 
@@ -13,6 +19,23 @@ public class Main {
         /*HQL using (Hibernate Query language)*/
         List fromStudent = session.createQuery("from Student").getResultList();
         System.out.println(fromStudent.size());
+
+        int id = 4;
+        String name = "Tharusha";
+        String address = "Panadura";
+        Laptop laptop1 = new Laptop(1,"HP");
+        Student student = new Student(id,name,address,laptop1);
+        String hql = "INSERT INTO Student (id, name, address,laptop) select ?1,?2,?3,?4 from Student ";
+        Query query = session.createQuery(hql);
+        query.setParameter(1,id);
+        query.setParameter(2,name);
+        query.setParameter(3,address);
+        query.setParameter(4,laptop1);
+        int i = query.executeUpdate();
+        System.out.println("Values  "+ i);
+
+
+
 
         /*SQL using*/
         List resultList = session.createNativeQuery("select  * from Student").getResultList();
